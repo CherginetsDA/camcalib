b69401add3f21d8cb6f7fd7efc5247bc4906a4d2
CherginetsDA <CherginetsDA@yandex.ru>
2020-01-21 22:03:46 +0300

    Create data set and a little bit changed script

--- scripts/calibration/data_create.py
+++ scripts/calibration/data_create.py
@@ -3,35 +3,46 @@ from datetime import datetime
 import os
 
 
-# Well, this script for creating data set. You just start it, look at screen and it you are pritty like that you see, just press S and this frame going to save in data/calibration/<data and time then it was created> folder.
+# Well, this script for creating data set. You just start it, look at screen and you'll see data from your camera, just press S and this frame going to save in data/calibration/<data and time then it was created> folder.
 # I hope I'll realized something much cooler then that (^.^)
 def main():
-    print("Press s to save image")
-    print("Press q to exit...")
     cap = cv2.VideoCapture(0)
-
+    print("Opening camera...")
     #check that camera is working
     if not cap.isOpened():
         cap.open()
         if not cap.isOpened():
             print("Can't find camera...")
             exit()
+    print("Done.")
+
+    print("Change image parameters...")
     # Set another size of capture
     cap.set(3,1080)
     cap.set(4,720)
+    print("Done.")
 
     path = os.getcwd() + '/../data/calibration/' + datetime.now().strftime("%d%m%Y%H%M%S") + '/'
     os.mkdir(path)
     pictNumber = 1
 
+
+    print("Press s to save image")
+    print("Press q to exit...")
+
     while(pictNumber < 26):
         ret,frame = cap.read()
-        cv2.imshow('Camera Test', frame)
+        cv2.imshow('Camera Data', frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('s'):
+        kin = cv2.waitKey(1)
+
+        if  kin == ord('s'):
             cv2.imwrite(path+str(pictNumber)+'.jpg',frame)
             print("Save picture number: " + str(pictNumber))
             pictNumber = pictNumber + 1
+        elif kin == ord('q'):
+            break
+
 
 
     cap.release()
@@ -45,3 +56,5 @@ if __name__ == '__main__':
         exit()
     except:
         print("Something is wrong")
+
+## Maybe if save image in list and save in the end of program, it's can be more comfortable.
