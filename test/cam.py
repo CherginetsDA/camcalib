26379ded2931b1d5f0554b716a982dc213b6d937
CherginetsDA <CherginetsDA@yandex.ru>
2020-05-29 01:44:34 +0300

    Start merge camcalib and ros youbot simulation. Camera test is work. Also I found and add funny cat image

--- test/cam.py
+++ test/cam.py
@@ -1,31 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import argparse
 import cv2
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
 
-def main():
-    print("Press q to exit...")
-    cap = cv2.VideoCapture(0)
+bridge = CvBridge()
+cv_image = cv2.imread('../image/cat.jpg')
 
-    if not cap.isOpened():
-        cap.open()
-        if not cap.isOpened():
-            print("Can't find camera...")
-            exit()
+def callback(image_message):
+    global cv_image
+    cv_image = bridge.imgmsg_to_cv2(image_message, desired_encoding='passthrough')
 
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/usb_cam/image_raw", Image, callback)
     while(True):
-        ret,frame = cap.read()
-        cv2.imshow('Camera Test', frame)
+        cv2.imshow('Camera Test', cv_image)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    cap.release()
     cv2.destroyAllWindows()
     print("Done.")
 
 if __name__ == '__main__':
     try:
-        main()
+        listener()
     except KeyboardInterrupt:
         exit()
-    except:
-        print("Something is wrong")
